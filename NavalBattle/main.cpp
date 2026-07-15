b154b24c53c7d39d8eb4a0a8efa181f4fbf24001
Merdan <wisew.tube@gmail.com>
2026-07-16 02:38:14 +0500

    Update main.cpp

--- NavalBattle/main.cpp
+++ NavalBattle/main.cpp
@@ -1,15 +1,32 @@
 ﻿#include "raylib.h"
 
-int main() {
-    const int screenWidth = 640;
-    const int screenHeight = 640;
+const int BoardSize = 8;
+const int CellSize = 80;
+const int ScreenWidth = BoardSize * CellSize;
+const int ScreenHeight = BoardSize * CellSize;
 
-    InitWindow(screenWidth, screenHeight, "Naval Battle");
+void DrawGrid() {
+    for (int row = 0; row < BoardSize; row++) {
+        for (int col = 0; col < BoardSize; col++) {
+            int x = col * CellSize;
+            int y = row * CellSize;
+
+            DrawRectangle(x, y, CellSize, CellSize, RAYWHITE);
+            DrawRectangleLines(x, y, CellSize, CellSize, DARKGRAY);
+        }
+    }
+}
+
+int main() {
+    InitWindow(ScreenWidth, ScreenHeight, "Naval Battle");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        DrawGrid();
+
         EndDrawing();
     }
 
