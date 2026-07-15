122bd415b08a975b65993b2d7a430ddace939b5a
Merdan <wisew.tube@gmail.com>
2026-07-16 03:01:16 +0500

    Update Board.h

--- NavalBattle/src/Board.h
+++ NavalBattle/src/Board.h
@@ -1,18 +1,34 @@
 #pragma once
 #include <array>
+#include <vector>
 #include "CellState.h"
+
+enum class Orientation {
+    Horizontal,
+    Vertical
+};
+
+struct ShipData {
+    int totalCells = 0;
+    int hitCells = 0;
+    bool sunk = false;
+};
 
 class Board {
 public:
-	static const int Size = 8;
+    static const int Size = 8;
 
-	Board();
+    Board();
 
-	bool PlaceShip(int row, int col);
-	bool Shoot(int row, int col);
-	CellState GetCell(int row, int col) const;
-	bool IsInBounds(int row, int col) const;
-	bool AllShipsSunk() const;
+    bool PlaceShip(int row, int col, int length, Orientation orientation);
+    bool Shoot(int row, int col, bool& outNewlySunk);
+    CellState GetCell(int row, int col) const;
+    bool IsInBounds(int row, int col) const;
+    bool IsCellPartOfSunkShip(int row, int col) const;
+    bool AllShipsSunk() const;
+
 private:
-	std::array<std::array<CellState, Size>, Size> grid;
+    std::array<std::array<CellState, Size>, Size> grid;
+    std::array<std::array<int, Size>, Size> shipId;
+    std::vector<ShipData> ships;
 };
